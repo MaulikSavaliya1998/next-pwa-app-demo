bc73227aa9eac2dac41577c6204c566d3d159041
Maulik Savaliya <savaliyamaulik98@gmail.com>
2024-06-26 15:44:17 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Next.js PWA APP</h1>
       <h1>Maulik Savaliya</h1>
     </main>
   );
