d254f968429670a8b4ef68af8db04f793835da6a
Maulik Savaliya <savaliyamaulik98@gmail.com>
2024-06-26 15:37:09 +0530

    Added new line

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Next.js PWA APP</h1>
+      <h1>Maulik Savaliya</h1>
     </main>
   );
 }
